6640fd6379d58883eabb23e5d65242637d26ca35
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-06 02:34:41 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,9 @@
 def starts_with_a_vowel?(word)
-  return true if word.scan(/\b[aeiouAEIOU]/)
-  return false if word.scan(/\b![aeiouAEIOU]/)
+  if word.scan(/\b[aeiouAEIOU]/)
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
