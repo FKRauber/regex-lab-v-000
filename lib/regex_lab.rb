55c17a3493e3d8495effda8380dbdfe95dafadef
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-06 02:42:21 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan()
+  #text.scan()
 end
 
 def words_five_letters_long(text)
@@ -11,9 +11,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(//) ? true : false
+  #text.match(//) ? true : false
 end
 
 def valid_phone_number?(phone)
-  phone.match(//) ? true : false
+  #phone.match(//) ? true : false
 end
